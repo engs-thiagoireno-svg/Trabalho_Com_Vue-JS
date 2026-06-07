47fb0f070e437904515e374697623c06e524af2b
thiago <engs-thiagoireno@camporeal.edu.br>
2026-06-07 13:57:13 -0300

    Estruturar atividades com o primeiro objeto do cronograma Credenciamento

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <script setup>
-    const ano = new Date().getFullYear()
+const ano = new Date().getFullYear()
 </script>
 
 <template>
---
+++ src/views/CronogramaView.vue
@@ -0,0 +1,11 @@
+<script setup>
+const atividades = [
+  {
+    horario: '09:00 - 09:30',
+    atividade: 'Credenciamento e lanche',
+    local: 'Salão Principal',
+    destaque: false,
+  },
+  
+]
+</script>
