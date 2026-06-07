1f39b1756ca6be84e7ee02b1d9afe81baf6c8a2f
thiago <engs-thiagoireno@camporeal.edu.br>
2026-06-07 15:20:02 -0300

    Colocando a barra de navegação e o rodapé fixos no arquivo principal com a troca de páginas no meio

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+import TheNavbar from './components/TheNavbar.vue'
+import TheFooter from './components/TheFooter.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <TheNavbar />
+  <router-view />
 
-<style scoped></style>
+  <TheFooter />
+</template>
