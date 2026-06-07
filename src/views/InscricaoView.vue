ab0e9f6f9442ca1e57592f0c9f22189654987d5a
Luis <engs-luiskomar@camporeal.edu.br>
2026-06-07 15:42:35 -0300

    formulario de inscricao com v-model

--- src/views/InscricaoView.vue
+++ src/views/InscricaoView.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { reactive, ref } from 'vue'
 
+const form = reactive({
+  nomeEquipe: '',
+  emailLider: '',
+  experiencia: 'iniciante',
+  acessibilidade: false,
+})
+
 
 
 
