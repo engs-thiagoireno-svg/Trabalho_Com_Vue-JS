0d17f2326fb3e3151817b0cebc41b54254833e63
thiago <engs-thiagoireno@camporeal.edu.br>
2026-06-07 14:41:07 -0300

    Criando a função que calcula e atualiza o tempo restante para o evento

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,5 +8,20 @@ const minutos = ref(0)
 
 let intervalo = null
 
+function atualizarContagem() {
+  const agora = new Date()
+  const diferenca = dataEvento - agora
+
+  if (diferenca <= 0) {
+    dias.value = 0
+    horas.value = 0
+    minutos.value = 0
+    return
+  }
+
+  dias.value = Math.floor(diferenca / (1000 * 60 * 60 * 24))
+  horas.value = Math.floor((diferenca / (1000 * 60 * 60)) % 24)
+  minutos.value = Math.floor((diferenca / (1000 * 60)) % 60)
+}
 
 </script>
