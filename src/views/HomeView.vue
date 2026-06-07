2699eae2a02d9509523cfff82f9c56ea5bcab4ad
thiago <engs-thiagoireno@camporeal.edu.br>
2026-06-07 14:35:48 -0300

    Iniciar script do cronômetro criando as variáveis para os dias, horas e minutos

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const dataEvento = new Date('2026-03-31T09:00:00')
+const dias = ref(0)
+const horas = ref(0)
+const minutos = ref(0)
+
+let intervalo = null
+
+
+</script>
