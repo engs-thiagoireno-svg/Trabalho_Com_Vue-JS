40c38b296c47c4438da53abdb663f994315b01a7
Luis <engs-luiskomar@camporeal.edu.br>
2026-06-07 21:59:51 -0300

    Criação das escritas do primeiro card

--- src/views/PalestrantesView.vue
+++ src/views/PalestrantesView.vue
@@ -1,3 +1,13 @@
 <script setup>
 import { ref, computed } from 'vue'
+
+const palestrantes = [
+  {
+    nome: 'Piazão do Front',
+    foto: 'https://media.licdn.com/dms/image/v2/D4D03AQGR59GSYZ1VQA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1706293541390?e=1782345600&v=beta&t=vtZQ8Db24IQ9LIE3b6H0xOC83BZiIGCOdiTJEKIXDB0',
+    especialidade: 'Especialista em Web',
+    descricao:
+      'Vai te ajudar a estruturar a lógica e o Front-End do seu projeto.',
+    }]
+
 </script>
