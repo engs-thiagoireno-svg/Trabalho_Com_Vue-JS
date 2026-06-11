4233777233df13ec427d13cb00fa3fdcacb03248
Bruno <engs-brunoroth@camporeal.edu.br>
2026-06-11 01:17:00 -0300

    cada rota aponta para uma view

--- src/router/index.js
+++ src/router/index.js
@@ -3,3 +3,10 @@ import HomeView from '../views/HomeView.vue'
 import CronogramaView from '../views/CronogramaView.vue'
 import PalestrantesView from '../views/PalestrantesView.vue'
 import InscricaoView from '../views/InscricaoView.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/cronograma', name: 'cronograma', component: CronogramaView },
+  { path: '/palestrantes', name: 'palestrantes', component: PalestrantesView },
+  { path: '/inscricao', name: 'inscricao', component: InscricaoView },
+]
